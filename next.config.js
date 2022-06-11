d1d298b19a45413faaf470ea10f8c1702c6ae3e0
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-11 14:51:55 +0200

    cors

--- next.config.js
+++ next.config.js
@@ -6,7 +6,15 @@ const nextConfig = {
   },
   env: {
     API_URL: process.env.API_URL,
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/api/:path*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
